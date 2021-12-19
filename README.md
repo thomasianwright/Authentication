acc9ca2b1be3ebd6f24c133d3f951bc245735ffb
Tom <tomianwright@gmail.com>
2021-12-19 04:32:47 +0000

    README.md & environment variable added to DB

---
+++ README.md
@@ -0,0 +1,11 @@
+# Auth API
+
+Created in GOLANG.
+
+# Setup
+
+## Environment Variables
+
+- connectionstring `root:rootroot@/auth`
+- jwtsecret `securestringhere`
+- port `:3000`
--- database/connection.go
+++ database/connection.go
@@ -4,12 +4,13 @@ import (
 	"authapi/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 )
 
 var DB *gorm.DB
 
 func Connect() {
-	conn, err := gorm.Open(mysql.Open("root:rootroot@/auth"), &gorm.Config{})
+	conn, err := gorm.Open(mysql.Open(os.Getenv("connection")), &gorm.Config{})
 
 	DB = conn
 
